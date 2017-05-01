5ffc46e3bddd5cb0e33aadfbe9de5a851151db41
Eric King <eking2688@erics-mbp.gateway.pace.com>
2017-05-01 17:01:59 -0400

    initial constructors

---
+++ ClozeCard.js
@@ -0,0 +1,23 @@
+function ClozeCard(text, cloze) {
+    if (!(this instanceof ClozeCard))
+        return new ClozeCard(text, cloze);
+    
+    this.fullText = text;
+    this.cloze = cloze;
+    // this.partial = text.replace(cloze, '...');
+}
+
+//USING STRING METHOD REPLACE TO REMOVE PART OF SENTENCE
+ClozeCard.prototype.partText = function () {
+    return this.text.replace(this.cloze, '..........');
+};
+// DISPLAY ONLY THE SOLUTION
+ClozeCard.prototype.clozeText = function () {
+    return this.cloze;
+};
+
+
+
+
+
+module.exports = ClozeCard;
---
+++ interface.js
@@ -0,0 +1,6 @@
+var inquirer = require('inquirer');
+var basicCard = require('./BasicCard.js');
+var clozeCard = require('./ClozeCard.js');
+
+var basicFlashCard = [];
+var clozeFlashCard = [];
